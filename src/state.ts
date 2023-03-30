c314d9ba26dd32f8f1d6f7af9f07aadb0308a4b8
Patrick Curran <patrickthebold@gmail.com>
2023-03-29 21:59:42 -0400

    eslint

--- src/state.ts
+++ src/state.ts
@@ -1,29 +1,28 @@
 // We define a reducer as a function of the form (state, ...data) => newState.
-// A handler is a function of the form (...data) => void. 
+// A handler is a function of the form (...data) => void.
 // Once an initial state is given we can turn reducers into handlers.
 // We can then supply a stream of states to anyone who subscribes.
 
 export const initState = <State>(initialState: State) => {
-    const subscribers: Set<(state: State) => void> = new Set();
-    let state: State = initialState;
-    const pushState = () => subscribers.forEach(sub => sub(state))
-    
-    const createHandler = <D extends unknown[]>(reducer: (s: State, ...args: D) => State) => (...data: D) => {
-        state = reducer(state, ...data)
-        pushState()
-    }
-    
-    const subscribe = (sub: (state: State) => void): void => {
-        subscribers.add(sub)
-    }
+  const subscribers = new Set<(state: State) => void>()
+  let state: State = initialState
+  const pushState = () => { subscribers.forEach(sub => { sub(state) }) }
 
-    const unsubscribe = (sub: (state: State) => void): void => {
-        subscribers.delete(sub)
-    }
-    return {
-        createHandler,
-        subscribe,
-        unsubscribe
-    }
+  const createHandler = <D extends unknown[]>(reducer: (s: State, ...args: D) => State) => (...data: D) => {
+    state = reducer(state, ...data)
+    pushState()
+  }
+
+  const subscribe = (sub: (state: State) => void): void => {
+    subscribers.add(sub)
+  }
+
+  const unsubscribe = (sub: (state: State) => void): void => {
+    subscribers.delete(sub)
+  }
+  return {
+    createHandler,
+    subscribe,
+    unsubscribe
+  }
 }
-
