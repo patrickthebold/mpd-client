b6225e289f764aa2984227b4e0947b0849c556a1
Patrick Curran <patrickthebold@gmail.com>
2023-03-29 22:05:41 -0400

    prettier

--- src/state.ts
+++ src/state.ts
@@ -4,25 +4,31 @@
 // We can then supply a stream of states to anyone who subscribes.
 
 export const initState = <State>(initialState: State) => {
-  const subscribers = new Set<(state: State) => void>()
-  let state: State = initialState
-  const pushState = () => { subscribers.forEach(sub => { sub(state) }) }
+  const subscribers = new Set<(state: State) => void>();
+  let state: State = initialState;
+  const pushState = () => {
+    subscribers.forEach((sub) => {
+      sub(state);
+    });
+  };
 
-  const createHandler = <D extends unknown[]>(reducer: (s: State, ...args: D) => State) => (...data: D) => {
-    state = reducer(state, ...data)
-    pushState()
-  }
+  const createHandler =
+    <D extends unknown[]>(reducer: (s: State, ...args: D) => State) =>
+    (...data: D) => {
+      state = reducer(state, ...data);
+      pushState();
+    };
 
   const subscribe = (sub: (state: State) => void): void => {
-    subscribers.add(sub)
-  }
+    subscribers.add(sub);
+  };
 
   const unsubscribe = (sub: (state: State) => void): void => {
-    subscribers.delete(sub)
-  }
+    subscribers.delete(sub);
+  };
   return {
     createHandler,
     subscribe,
-    unsubscribe
-  }
-}
+    unsubscribe,
+  };
+};
