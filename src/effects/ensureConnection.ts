41973edfa1e31f40f75c7009496834dc6d30d737
Patrick Curran <patrickthebold@gmail.com>
2023-07-24 07:16:44 -0400

    handle messages from ws (#4)

--- src/effects/ensureConnection.ts
+++ src/effects/ensureConnection.ts
@@ -41,6 +41,21 @@ const setConnected = createHandler((state) => {
   }
 });
 
+const handleMessage = createHandler((state, e: MessageEvent) => {
+  if (state.websocketStatus !== "connected") {
+    console.error("Got a message when not connected");
+    return state;
+  }
+  const probablyText = e.data;
+  if (typeof probablyText === "string") {
+    return state.update("responseData", (response) =>
+      response.push(...probablyText.split("\n"))
+    );
+  }
+  console.warn("We did not get a string over the websocket");
+  return state;
+});
+
 export const ensureConnection: Effect = (state) => {
   if (
     state.websocketStatus === "disconnected" &&
@@ -52,6 +67,7 @@ export const ensureConnection: Effect = (state) => {
     ws.addEventListener("close", setDisconnected);
     ws.addEventListener("open", setConnected);
     ws.addEventListener("error", setDisconnected);
+    ws.addEventListener("message", handleMessage);
     return makeConnectingState({
       ...state.toObject(),
       websocketStatus: "connecting",
@@ -66,6 +82,7 @@ export const removeWsListeners: Effect = (state) => {
     state.ws.removeEventListener("close", setDisconnected);
     state.ws.removeEventListener("open", setConnected);
     state.ws.removeEventListener("error", setDisconnected);
+    state.ws.removeEventListener("message", handleMessage);
     state.remove("ws");
   }
   return state;
